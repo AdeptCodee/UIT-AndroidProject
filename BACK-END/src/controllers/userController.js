25cca7bf69428801c20e32eebb531182ec62f6e5
AdeptR <truongmanhnguyena@gmail.com>
2026-04-30 22:05:39 +0700

    back end done

--- BACK-END/src/controllers/userController.js
+++ BACK-END/src/controllers/userController.js
@@ -1,10 +1,9 @@
 export const authMe = async (req, res) => {
-    try {
-        const user = req.user; // Lấy thông tin user đã được xác thực từ authMiddleware
-        return res.status(200).json({ user });
-         
-    } catch (error) {
-        console.error("Đã xảy ra lỗi khi gọi hàm authMe.", error);
-        return res.status(500).json({ message: "Đã xảy ra lỗi máy chủ!" });
-    }
-}
+  try {
+    const user = req.user; // Lấy thông tin user đã được xác thực từ authMiddleware
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Đã xảy ra lỗi khi gọi hàm authMe.", error);
+    return res.status(500).json({ message: "Đã xảy ra lỗi máy chủ!" });
+  }
+};
