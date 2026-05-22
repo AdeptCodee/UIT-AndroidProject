3cd184df8d7c3c7a28cd9769aca0311ce1bca2e1
AdeptR <truongmanhnguyena@gmail.com>
2026-05-22 21:50:26 +0700

    login ui

--- android-FRONT-END/app/build.gradle.kts
+++ android-FRONT-END/app/build.gradle.kts
@@ -43,4 +43,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // 1. Gọi API (Retrofit) - Vũ khí này thay thế hoàn toàn cho 'axios' bên Web của bạn
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    // 2. Real-time (Socket.io) - Giúp nhắn tin nhảy liên tục không cần tải lại trang
+    implementation("io.socket:socket.io-client:2.1.0")
+
+    // 3. Hiển thị ảnh (Glide) - Xử lý việc tải ảnh từ Cloudinary về máy mượt mà
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // 4. Giao diện (CircleImageView) - Công cụ hỗ trợ cắt ảnh đại diện thành hình tròn siêu nhanh
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
